9c335ba945f59f3ebf2b6b97a67b151e9f8b0e6a
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-01-25 11:34:51 +0200

    add docs and fake context

---
+++ Packages/xyz.vasd.fake/Runtime/index.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xyz.Vasd.Fake
+{
+    public class index {}
+
+    // stores data
+    public class FakeContext : MonoBehaviour
+    {
+        private Dictionary<Type, object> _values;
+
+        public bool Contains<T>()
+        {
+            return Contains(typeof(T));
+        }
+        public bool Contains(Type type)
+        {
+            return _values.ContainsKey(type);
+        }
+
+        public T Get<T>()
+        {
+            return (T)Get(typeof(T));
+        }
+        public object Get(Type type)
+        {
+            if (!_values.ContainsKey(type)) return null;
+            return _values[type];
+        }
+
+        public void Set<T>(T value)
+        {
+            Set(typeof(T), value);
+        }
+        public void Set(Type type, object value)
+        {
+            _values[type] = value;
+        }
+
+        public void Remove<T>()
+        {
+            Remove(typeof(T));
+        }
+        public void Remove(Type type)
+        {
+            _values.Remove(type);
+        }
+    }
+}
