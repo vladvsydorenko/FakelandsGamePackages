d54dfa5eb43236e6898786e4a8a09824cd0898fd
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-01 17:40:01 +0200

    Update FakeViewSwitch.cs

--- Packages/xyz.vasd.fake/Runtime/Views/FakeViewSwitch.cs
+++ Packages/xyz.vasd.fake/Runtime/Views/FakeViewSwitch.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Xyz.Vasd.Fake.Views
 {
@@ -20,5 +21,43 @@ namespace Xyz.Vasd.Fake.Views
             if (_current != null && !_current.OpenView()) return;
             if (_previous != null) _previous.CloseView();
         }
+    }
+
+    public class FakeViewRoute : MonoBehaviour
+    {
+        public string Location;
+    }
+
+    public class FakeViewRouter : MonoBehaviour
+    {
+        public string Location;
+
+        private IFakeView[] _currentViews;
+        private IFakeView[] _previousViews;
+
+        private string _currentLocation;
+        private Dictionary<string, IFakeView[]> _routes;
+
+        private void Awake()
+        {
+            var routes = GetComponentsInChildren<FakeViewRoute>();
+            foreach (var route in routes)
+            {
+                var views = route.gameObject.GetComponents<IFakeView>();
+                _routes[route.Location] = views;
+            }
+        }
+
+        private void Update()
+        {
+            if (Location == _currentLocation) return;
+
+            var views = _routes[Location];
+            foreach (var view in views)
+            {
+                view.OpenView();
+            }
+        }
+
     }
 }
