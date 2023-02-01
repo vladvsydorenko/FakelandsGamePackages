455ae25d3b729743a3536c12ea9be43dcac59441
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-01 04:28:51 +0200

    update

--- Packages/xyz.vasd.fake/Runtime/Views/FakeView.cs
+++ Packages/xyz.vasd.fake/Runtime/Views/FakeView.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace Xyz.Vasd.Fake
 {
@@ -27,18 +28,62 @@ namespace Xyz.Vasd.Fake
 
     public class AnimatedFakeView : FakeView, IFakeView
     {
-        
+        public PlayableAsset OpenTimeline;
+        public PlayableAsset IdleTimeline;
+        public PlayableAsset CloseTimeline;
+
+        public PlayableDirector Director;
 
         bool IFakeView.CloseView()
         {
-            gameObject.SetActive(false);
-            return true;
+            if (Director == null || CloseTimeline == null)
+            {
+                gameObject.SetActive(false);
+                return true;
+            };
+
+            Director.playableAsset = OpenTimeline;
+            Director.time = 0f;
+            Director.RebuildGraph();
+            Director.Play();
+
+            var visibility = Director.time >= Director.duration;
+            gameObject.SetActive(visibility);
+            return visibility;
         }
 
         bool IFakeView.OpenView()
         {
             gameObject.SetActive(true);
-            return true;
+
+            if (Director == null || OpenTimeline == null) return true;
+
+            Director.playableAsset = OpenTimeline;
+            Director.time = 0f;
+            Director.RebuildGraph();
+            Director.Play();
+
+            return Director.time >= Director.duration;
+        }
+    }
+
+    public class FakeViewSwitch : MonoBehaviour
+    {
+        private IFakeView _current;
+        private IFakeView _previous;
+
+        public void OpenView(IFakeView view)
+        {
+            if (_current == view) return;
+
+            _previous = _current;
+            _current = view;
+        }
+
+        private void Update()
+        {
+            if (_current != null || !_current.OpenView()) return;
+            if (_previous != null) _previous.CloseView();
         }
     }
 }
