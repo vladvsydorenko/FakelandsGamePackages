8bfc4531866000cf8c0f794a97742ca9e7a59fe5
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-02 16:03:45 +0200

    update

---
+++ Packages/xyz.vasd.fakelands/Runtime/AddressableLoader.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Xyz.Vasd.Fakelands
+{
+    [AddComponentMenu("Fakelands/[Fakelands] Addressable Loader")]
+    public class AddressableLoader : MonoBehaviour
+    {
+        private enum LoadStatus
+        {
+            Unloaded,
+            Loading,
+            Loaded
+        }
+
+        public AssetLabelReference[] Labels;
+        public AssetReference[] References;
+
+        private LoadStatus _status;
+        private List<AsyncOperationHandle> _handles = new();
+
+        public bool LoadAddressables()
+        {
+            var loaded = false;
+
+            // already loaded            
+            if (_status == LoadStatus.Loaded)
+            {
+                loaded = true;
+            }
+            
+            // was not loaded yet
+            else if (_status == LoadStatus.Unloaded)
+            {
+                LoadAddressables(Labels);
+                LoadAddressables(References);
+                
+                _status = LoadStatus.Loading;
+
+                loaded = false;
+            }
+
+            // loading is in progress
+            else if (_status == LoadStatus.Loading)
+            {
+                if (IsLoaded()) _status = LoadStatus.Loaded;
+
+                loaded = _status == LoadStatus.Loaded;
+            }
+
+            return loaded;
+        }
+
+        private void LoadAddressables(object[] keys)
+        {
+            foreach (var key in keys)
+            {
+                if (key == null) continue;
+
+                var handle = Addressables.DownloadDependenciesAsync(key);
+                _handles.Add(handle);
+            }
+        }
+
+        private bool IsLoaded()
+        {
+            return _handles.All(handle => handle.IsDone);
+        }
+    }
+}
---
+++ Packages/xyz.vasd.fakelands/Runtime/LoaderPage.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Xyz.Vasd.Fake.Views;
+
+namespace Xyz.Vasd.Fakelands
+{
+    [AddComponentMenu("Fakelands/[Fakelands] Preloader Page")]
+    public class LoaderPage : FakeAnimatedView
+    {
+        [Header("Refs")]
+        public FakeRouter Router;
+        public AddressableLoader Loader;
+
+        [Header("Settings")]
+        [Tooltip("Redirect router after loading. empty = disabled")]
+        public string Redirect;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Refresh();
+        }
+
+        protected virtual void Start()
+        {
+            if (Loader != null) Loader.LoadAddressables();
+        }
+
+        protected virtual void Update()
+        {
+            if (Loader != null && !Loader.LoadAddressables()) return;
+
+            if (Router != null &&
+                ViewStatus == FakeViewStatus.Open &&
+                Redirect.Length > 0)
+            {
+                Router.Location = Redirect;
+            }
+        }
+
+        protected override void OnValidate()
+        {
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            if (Router == null) Router = GetComponentInParent<FakeRouter>();
+            if (Loader == null) Loader = GetComponentInChildren<AddressableLoader>();
+        }
+    }
+}
