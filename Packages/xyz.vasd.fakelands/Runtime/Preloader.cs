9eb4c6127f174ce075097e9e736ee0421a6937e0
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-02 09:05:37 +0200

    update

---
+++ Packages/xyz.vasd.fakelands/Runtime/Preloader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Xyz.Vasd.Fake.Views;
+
+namespace Xyz.Vasd.Fakelands
+{
+    [AddComponentMenu("Fakelands/[Fakelands] Preloader")]
+    public class Preloader : MonoBehaviour
+    {
+        public FakeRouter Router;
+        private IFakeView LoaderView;
+
+        private void Awake()
+        {
+            Refresh();
+        }
+
+        private void OnValidate()
+        {
+            Refresh();
+        }
+
+        private void Update()
+        {
+            if (LoaderView.GetViewStatus() == FakeViewStatus.Open) Router.Location = "/loader";
+        }
+
+        private void Refresh()
+        {
+            LoaderView = GetComponent<IFakeView>();
+            if (Router == null) Router = GetComponentInParent<FakeRouter>();
+        }
+    }
+}
