046f53f0305bb0b35daeaf7881df7f356e7be991
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-03 23:22:25 +0200

    update

---
+++ Packages/xyz.vasd.fake/Runtime/Node.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xyz.Vasd.Fake
+{
+    public interface INode
+    {
+        int GetId();
+
+        INode GetParent();
+        void SetParent(INode node);
+
+        INode[] GetChildren();
+        void AddChild(INode child);
+        void RemoveChild(INode child);
+    }
+
+    public class Node : INode
+    {
+        public readonly int Id;
+
+        public Node Parent { get; private set; }
+
+        private List<INode> _children = new();
+        private int _childrenVersion;
+
+        private INode[] _childrenArray = new INode[0];
+        private int _childrenArrayVersion;
+
+        public Node(int id, Node parent = null)
+        {
+            Id = id;
+            Parent = parent;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        #region parent
+        public INode GetParent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetParent(INode node)
+        {
+            
+        }
+        #endregion
+
+        #region Children
+        public INode[] GetChildren()
+        {
+            if (_childrenArrayVersion != _childrenVersion)
+            {
+                _childrenArray = _children.ToArray();
+                _childrenArrayVersion = _childrenVersion;
+            }
+
+            return _childrenArray;
+        }
+
+        public void AddChild(INode child)
+        {
+            _children.Add(child);
+            _childrenVersion++;
+        }
+
+        public void RemoveChild(INode child)
+        {
+            _children.Remove(child);
+            _childrenVersion++;
+        }
+        #endregion
+    }
+}
