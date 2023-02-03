467dc8b35cc62ab5dce560796e48d35ccccc177d
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-03 18:17:10 +0200

    Update index.cs

--- Packages/xyz.vasd.fake/Runtime/index.cs
+++ Packages/xyz.vasd.fake/Runtime/index.cs
@@ -1,5 +1,9 @@
 //namespace Xyz.Vasd.Fake { public class index {} }
 
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Xyz.Vasd.Fake
 {
 
@@ -10,13 +14,61 @@ namespace Xyz.Vasd.Fake
 // Components
 namespace Xyz.Vasd.Fake
 {
-    public class Route
+    public interface INode
     {
-        public string Path;
+        void SetParent(INode node);
+        void AddChild(INode child);
+        void RemoveChild(INode child);
     }
 
-    public class Router
+    public class Node : INode
     {
-        public string Location;
+        void INode.SetParent(INode node)
+        {
+        }
+
+        void INode.AddChild(INode child)
+        {
+        }
+
+        void INode.RemoveChild(INode child)
+        {
+        }
+    }
+
+    public class Route : MonoBehaviour
+    {
+        public string Path { get; private set; }
+    
+        public Route ParentRoute { get; private set; }
+        public List<Route> ChildRoutes { get; private set; }
+
+        private void Awake()
+        {
+            var parentRoute = transform.parent.GetComponentInParent<Route>();
+
+            if (parentRoute == null)
+            {
+                // CASE: no parent route
+                return;
+            }
+
+            parentRoute.AddChildRoute(this);
+        }
+
+        public void SetParentRoute(Route parent)
+        {
+            ParentRoute = parent;
+        }
+
+        public void AddChildRoute(Route route)
+        {
+            ChildRoutes?.Add(route);
+        }
+
+        public void RemoveChildRoute(Route route)
+        {
+            ChildRoutes?.Remove(route);
+        }
     }
 }
