815cea9335c9b0595b8891c67ef802ebbc4223ae
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-03 16:40:49 +0200

    update

--- Packages/xyz.vasd.fake/Runtime/index.cs
+++ Packages/xyz.vasd.fake/Runtime/index.cs
@@ -1,25 +1,5 @@
-using UnityEngine;
-using UnityEngine.AddressableAssets;
-
 //namespace Xyz.Vasd.Fake { public class index {} }
 
 namespace Xyz.Vasd.Fake
 {
-    public enum FakeActionStage
-    {
-        None,
-        Start,
-        Work,
-        Done,
-    }
-
-    public class FakeAsset
-    {
-        public FakeActionStage LoadingStage { get; private set; }
-
-        public bool Load()
-        {
-            return LoadingStage == FakeActionStage.Done;
-        }
-    }
 }
