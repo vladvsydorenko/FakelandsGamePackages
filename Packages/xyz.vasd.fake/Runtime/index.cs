a267db3fba9ee3cd9deaf74f36ee6d8de21a63ec
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-03 17:54:49 +0200

    Update index.cs

--- Packages/xyz.vasd.fake/Runtime/index.cs
+++ Packages/xyz.vasd.fake/Runtime/index.cs
@@ -6,3 +6,17 @@ namespace Xyz.Vasd.Fake
     
 
 }
+
+// Components
+namespace Xyz.Vasd.Fake
+{
+    public class Route
+    {
+        public string Path;
+    }
+
+    public class Router
+    {
+        public string Location;
+    }
+}
