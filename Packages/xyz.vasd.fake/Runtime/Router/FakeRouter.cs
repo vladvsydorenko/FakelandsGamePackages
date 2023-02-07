0b7594797257f32f7ab586f69bdc7a97adbae283
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-02-07 02:34:06 +0200

    #2: basic router

---
+++ Packages/xyz.vasd.fake/Runtime/Router/FakeRouter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Xyz.Vasd.Fake.Router
+{
+    public class FakeRouter : MonoBehaviour
+    {
+        public string Path { get; private set; }
+        public string PreviousPath { get; private set; }
+
+        public List<IFakeRoute> Routes { get; private set; }
+
+        private List<IFakeRoute> _openRoutes;
+        private List<IFakeRoute> _closeRoutes;
+
+        private bool _isOpen;
+        private bool _isBusy;
+
+        [Header("Debug")]
+        [SerializeField]
+        private string _debugPath;
+
+        private void Awake()
+        {
+            Routes = GetComponentsInChildren<IFakeRoute>(includeInactive: true).ToList();
+        }
+
+        public void SetPath(string path)
+        {
+            if (_isBusy || Path == path) return;
+
+            var routes = Routes.FindAll(r => r.MatchRoute(path));
+            if (routes.Count < 1) return;
+
+            PreviousPath = Path;
+            Path = path;
+
+            _isBusy = true;
+            _isOpen = false;
+
+            _closeRoutes = _openRoutes;
+            _openRoutes = routes;
+        }
+
+        public void Update()
+        {
+            if (_debugPath.Length > 0) SetPath(_debugPath);
+                
+            if (!_isBusy) return;
+
+            if (!_isOpen) _isOpen = _openRoutes.All(route => route.OpenRoute());
+            if (!_isOpen) return;
+
+            _isBusy = _closeRoutes != null && !_closeRoutes.All(route => route.CloseRoute());
+        }
+    }
+}
--- Packages/xyz.vasd.fake/Runtime/Router/index.cs
+++ Packages/xyz.vasd.fake/Runtime/Router/index.cs
@@ -1,46 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using Xyz.Vasd.Fake.Task;
 
 //namespace Xyz.Vasd.Fake.Router{class index{}}
 
 namespace Xyz.Vasd.Fake.Router
 {
-    public interface IRoute
-    {
-        string GetPath();
-
-        bool Open();
-        bool Close();
-    }
-
-    public class Router
-    {
-        private List<IRoute> _routes = new();
-
-        public void AddRoute(IRoute route)
-        {
-            _routes.Add(route);
-        }
-
-        public void Process(string location)
-        {
-            var uri = new Uri(location);
-            IRoute foundRoute = null;
-
-            foreach (var route in _routes)
-            {
-                var parts = route.GetPath().Split('/');
-
-                var len = Mathf.Min(parts.Length, uri.Segments.Length);
-                for (int i = 0; i < len; i++)
-                {
-                    var part = parts[i];
-                    var uriPart = uri.Segments[i];
-                }
-            }
-        }
-    }
 }
